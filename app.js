568421f44d3f93068c0e43117057f4d101c3b072
isaaco1 <isaacorzech@gmail.com>
2023-08-29 23:44:02 +0100

    added basic JS Strings and Variables

---
+++ app.js
@@ -0,0 +1,15 @@
+const myString = "I'm a string";
+const myIntegerNumber = 23; // Number
+const myFloatNumber = 23.5; // Number
+const myBool = true; //can be false
+const myIntegerArray = [1, 2, 3, 4, 5];
+const myStringArray = ["doll", "racecar", "ball"]; // aka List
+const myObject = { key: "value", otherKey: "otherValue" }; // aka Dictionary
+
+console.log(myString);
+
+const myFirstName = "Isaac";
+const myLastName = "Orzech";
+const myFullName = myFirstName + " " + myLastName;
+
+console.log(myFullName);
